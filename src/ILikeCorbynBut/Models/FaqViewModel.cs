252ece0d5a370d5f2ad8617313915a4c0e739ce3
David Thompson <davyt23@hotmail.co.uk>
2017-04-25 17:06:06 +0100

    Initial checkin

---
+++ src/ILikeCorbynBut/Models/FaqViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ILikeCorbynBut.Models
+{
+    public class FaqViewModel
+    {
+        [Required]
+        public string DivId { get; set; }
+
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public string Answer { get; set; }
+
+        public bool Publish { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+        public ApplicationUser Reviewer { get; set; }
+
+        [Required]
+        public DateTime SubmittedOn { get; set; }
+    }
+}
